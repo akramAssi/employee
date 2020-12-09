02bf261bc78a15b2f8892582a874718fca87f769
akramassi <akram.20002010@gmail.com>
2020-12-09 07:43:11 +0200

    add method insert to database

--- app/src/main/java/com/example/employee/myDataBase.java
+++ app/src/main/java/com/example/employee/myDataBase.java
@@ -16,6 +16,7 @@ public class myDataBase {
         this.db = db;
         db.execSQL("create table if not exists emp(id Integer primary key , name varchar(15),sex char(1),BaseSalary float , TotalSalary float,CommissionRate float);");
     }
+
     public ArrayList<emp> getAllData()
     {
         try
@@ -38,42 +39,43 @@ public class myDataBase {
         catch (Exception ex)
         {
             Log.e("catchError", Objects.requireNonNull(ex.getMessage()));
+            return null;
         }
         return getData(rs);
     }
-    public ArrayList<emp> search(String name)
-    {
+
+    public ArrayList<emp> search(String name) {
 
         try {
-            rs = db.rawQuery("select * from emp where name LIKE '%"+name+"%' ", null);
-        }
-        catch (Exception ex)
-        {
+            rs = db.rawQuery("select * from emp where name LIKE '%" + name + "%' ", null);
+        } catch (Exception ex) {
             Log.e("catchError", Objects.requireNonNull(ex.getMessage()));
         }
         return getData(rs);
     }
 
-    public void delete(int id)
-    {
+    public void insert(Object[] info) {
+        db.execSQL("insert into emp values(?,?,?,?,?,?);", info);
+    }
+
+    public void delete(int id) {
         db.delete("emp", "id = ?", new String[]{"" + id});
     }
-    public void update(int id, float salary, float sales, float rate)
-    {
-        ContentValues vs=new ContentValues();
-        vs.put("BaseSalary",salary);
-        vs.put("TotalSalary",sales);
-        vs.put("CommissionRate",rate);
+
+    public void update(int id, float salary, float sales, float rate) {
+        ContentValues vs = new ContentValues();
+        vs.put("BaseSalary", salary);
+        vs.put("TotalSalary", sales);
+        vs.put("CommissionRate", rate);
         db.update("emp", vs, "id = " + id, null);
     }
-    private ArrayList<emp> getData(Cursor rs)
-    {
+
+    private ArrayList<emp> getData(Cursor rs) {
         ArrayList<emp> employee;
         employee = new ArrayList<>();
 
-        while (rs.moveToNext())
-        {
-            emp person=new emp();
+        while (rs.moveToNext()) {
+            emp person = new emp();
             person.setId(rs.getInt(0));
             person.setName(rs.getString(1));
             person.setGender(rs.getString(2).equals("m") ? "Male" : "Female");
